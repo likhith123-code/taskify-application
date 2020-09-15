6d2f973a9e5ab866531e45b42b649eec369acb97
unknown <likhith2901@gmail.com>
2020-09-15 21:20:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,45 @@ import TodoInput from './components/TodoInput';
 
 
 function App() {
+  state={
+    items:[{id:1,title:"Wakeup"},{id:2,title:"Make breakfast"}],
+    id:uuid(),
+    item:'',
+    editItem:false
+  };
+  handleChange=(e)=>{
+
+  }
+  handleSubmit=(e)=>{
+
+  }
+  clearList=()=>{
+
+  }
+  handleDelete=(id)=>{
+
+  }
+  handleEdit=(id)=>{
+
+  }
   return (
-    <div className="App">
-     <h1>Hello </h1>
-     <div className="container">
-       <div className="row">
-         
-         <TodoInput/>
-         <TodoList/>
-         
-       </div>
-     </div>
+    <div className="Container">
+      <div className="row">
+        <div className="col-10 col-md-8 mt-5 mx-auto">
+          <h3 className="text-capitalize text-center">Todo Input</h3>
+          <TodoInput 
+          item={this.state.item}
+          handleChange={this.handleChange} 
+          handleSubmit={this.handleSubmit}
+          editItem={this.editItem}/>
+          <TodoList
+          items={this.state.items}
+          clearList={this.clearList}
+          handleDelete={this.handleDelete}
+          handleEdit={this.handleEdit}/>
+        </div>
+      </div>
+      
     </div>
   );
 }
